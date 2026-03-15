e12512bf711a03f9dc3f631f8dd7c095f427148c
JiaYiBee07 <145582435+JiaYiBee07@users.noreply.github.com>
2026-03-15 19:53:55 +0800

    Update proxy.js 2

--- aether_proxy/api/proxy.js
+++ aether_proxy/api/proxy.js
@@ -1,15 +1,17 @@
 export default async function handler(req, res) {
-  const { path = "" } = req.query;
-
-  const backendUrl = "http://149.118.151.140:8080/api/air-quality/" + path;
-
   try {
+    const queryString = new URLSearchParams(req.query).toString();
+
+    const backendUrl =
+      "http://149.118.151.140:8080/api/air-quality/real-time/air-quality?" +
+      queryString;
+
     const response = await fetch(backendUrl);
     const data = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(data);
+    res.status(response.status).send(data);
   } catch (error) {
-    res.status(500).json({ error: "Proxy request failed" });
+    res.status(500).json({ error: "Proxy failed" });
   }
 }
